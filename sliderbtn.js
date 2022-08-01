77e01140a86774f1f86ea9beea8088d83185b636
Aleksandra2806 <k.sasha9636@mail.com>
2022-08-01 20:50:29 +0300

    commit slider1

--- sliderbtn.js
+++ sliderbtn.js
@@ -1,35 +1,21 @@
-let sliderContainer = document.getElementById('sliderContainer').offsetWidth;
-let btnNext = document.getElementById('next');
-let btnPrev = document.getElementById('prev');
-let slider = document.querySelector('div.slider');
-let btnsSlide = document.querySelectorAll('.btnSlide');
-let btnSlide = 0;
-
-btnsSlide[0].style.backgroundColor = "grey";
-
-function nextPage() {
-    btnsSlide[btnSlide].style.backgroundColor = "black";
-    if (btnSlide < 4) {
-        btnSlide++;
-    } else {
-        btnSlide = 0;
-    }
-    btnsSlide[btnSlide].style.backgroundColor = "grey";
-    slider.style.left = -btnSlide * sliderContainer + "px";
-    
-}
-btnNext.addEventListener('click', nextPage)
-
-function prevPage() {
-    btnsSlide[btnSlide].style.backgroundColor = "black";
-    if (btnSlide > 0) {
-        btnSlide--;
-    } else {
-        viewSlide = 4;
-    }
-    btnsSlide[btnSlide].style.backgroundColor = "grey";
-    slider.style.left = -btnSlide * sliderContainer+ "px";
-}
-
-btnPrev.addEventListener('click', prevPage)
-
+const slider = {
+	slides: ['./img/fon.jpg','./img/fon2.jpg','./img/fon3.jpg','./img/fon.jpg','./img/fon2.jpg'],
+	i: 0, 
+	set: function(image) {
+		document.querySelector(".slide").style.backgroundImage = "url("+image+")";
+	},
+	init: function() { 
+		this.set(this.slides[this.i]);
+	},
+	left: function() { 
+		this.i--;
+		if(this.i < 0) this.i = this.slides.length-1;
+		this.set(this.slides[this.i]);
+	},
+	right: function() { 
+		this.i++;
+		if(this.i == this.slides.length) this.i = 0;
+		this.set(this.slides[this.i]);		
+	}
+};
+slider.init();
